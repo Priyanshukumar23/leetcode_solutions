ecca2efcb42117c9d78c91ed60c82def3740b186
Priyanshu kumar <152582789+Priyanshukumar23@users.noreply.github.com>
2026-02-09 13:00:00 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (96.57%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -1,20 +1,31 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
-        int n = tickets.length;
-        int s = 0;
-        int i=0;
-        while(tickets[k]!=0){
+    //     int n = tickets.length;
+    //     int s = 0;
+    //     int i=0;
+    //     while(tickets[k]!=0){
             
-            if(i==n){
-                i=0;
-            }
-            if(tickets[i]>0){
+    //         if(i==n){
+    //             i=0;
+    //         }
+    //         if(tickets[i]>0){
 
-            tickets[i] -= 1;
-            s++;
+    //         tickets[i] -= 1;
+    //         s++;
+    //         }
+    //         i++;
+    //     }
+    // return s;
+     int time = 0;
+        int target = tickets[k];
+
+        for (int i = 0; i < tickets.length; i++) {
+            if (i <= k) {
+                time += Math.min(tickets[i], target);
+            } else {
+                time += Math.min(tickets[i], target - 1);
             }
-            i++;
         }
-    return s;
+        return time;
     }
 }

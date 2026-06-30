09b3eda5092b06049745d6339b0226e571c71138
Priyanshu kumar <152582789+Priyanshukumar23@users.noreply.github.com>
2026-06-30 11:31:15 +0530

    Time: 0 ms (100.00%) | Memory: 47.9 MB (57.08%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,12 +1,17 @@
 class Solution {
-    // public static double recur(double x, int n){
+    // public static double recur(double x, long n){
     //     if(n==0)return 1;
     //     x *= recur(x,n-1);
+    //     // return x*recur(x,n-1);
     //     return x;
 
     // }
     public double myPow(double x, int n) {
-        double z = Math.pow(x,n);
-        return z;
+    //    long N = n;
+    //    if(N<0){
+    //     x= 1/x;
+    //     N = -N;
+    //    }
+        return Math.pow(x,n);
     }
 }

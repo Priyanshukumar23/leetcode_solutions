33f2f94cbfabbc044fb3fee167a2156c46cdf63f
Priyanshu kumar <152582789+Priyanshukumar23@users.noreply.github.com>
2026-06-30 11:20:14 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (22.21%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,12 @@
+class Solution {
+    // public static double recur(double x, int n){
+    //     if(n==0)return 1;
+    //     x *= recur(x,n-1);
+    //     return x;
+
+    // }
+    public double myPow(double x, int n) {
+        double z = Math.pow(x,n);
+        return z;
+    }
+}

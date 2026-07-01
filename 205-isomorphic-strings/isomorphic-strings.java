b8fd0dde38f92f2af8641b044574a932a244e767
Priyanshu kumar <152582789+Priyanshukumar23@users.noreply.github.com>
2026-07-01 11:10:09 +0530

    Time: 24 ms (12.57%) | Memory: 43.9 MB (42.26%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -3,22 +3,6 @@ class Solution {
         int n = s.length();
         int y = t.length();
         if(n!=y)return false;
-        // HashMap<Character,Integer>mp = new HashMap<>();
-        // HashMap<Character,Integer>mp2 = new HashMap<>();
-        // for(int i=0;i<n;i++){
-        //     char ch = s.charAt(i);
-        //     char cl = t.charAt(i);
-        //     mp.put(ch,mp.getOrDefault(ch,0)+1);
-        //     mp2.put(cl,mp2.getOrDefault(cl,0)+1);
-        // }
-        // for(int i=0;i<n;i++){
-        //     char ch = s.charAt(i);
-        //     char cl = t.charAt(i);
-        //     if(mp.get(ch)!=mp2.get(cl)){
-        //         return false;
-        //     }
-        // }
-        // return true;
         HashMap<Character,Character>mp = new HashMap<>();
         HashMap<Character,Character>mp2 = new HashMap<>();
         for(int i=0;i<n;i++){

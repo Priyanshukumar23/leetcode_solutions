07e5ceedbb71c1d21e10be14015dd684785b3abe
Priyanshu kumar <152582789+Priyanshukumar23@users.noreply.github.com>
2026-07-01 09:43:40 +0530

    Time: 19 ms (33.18%) | Memory: 111.1 MB (5.72%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,37 +1,37 @@
-class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        int n = nums.length;
-        boolean c = false;
-        HashMap<Integer,Integer>mp = new HashMap<>();
-        for(int i=0;i<n;i++){
-            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
-            
-        }
-        for(int i=0;i<n;i++){
-            if(mp.get(nums[i])>1){
-                c= true;
-            }
-            
-        }
-            return c;
-    }
-}
-
-
 // class Solution {
 //     public boolean containsDuplicate(int[] nums) {
-
-//         HashMap<Integer, Integer> mp = new HashMap<>();
-
-//         for (int num : nums) {
-
-//             if (mp.containsKey(num)) {
-//                 return true;
-//             }
-
-//             mp.put(num, 1);
+//         int n = nums.length;
+//         boolean c = false;
+//         HashMap<Integer,Integer>mp = new HashMap<>();
+//         for(int i=0;i<n;i++){
+//             mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
+            
 //         }
-
-//         return false;
+//         for(int i=0;i<n;i++){
+//             if(mp.get(nums[i])>1){
+//                 c= true;
+//             }
+            
+//         }
+//             return c;
 //     }
 // }
+
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        HashMap<Integer, Integer> mp = new HashMap<>();
+
+        for (int num : nums) {
+
+            if (mp.containsKey(num)) {
+                return true;
+            }
+
+            mp.put(num, 1);
+        }
+
+        return false;
+    }
+}

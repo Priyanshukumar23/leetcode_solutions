ab0edc0614437b60141228e06d1fc57e3c5d78f9
Priyanshu kumar <152582789+Priyanshukumar23@users.noreply.github.com>
2026-07-02 09:58:18 +0530

    Time: 13 ms (75.06%) | Memory: 46.5 MB (65.03%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,53 +1,55 @@
 class Solution {
     public String frequencySort(String s) {
         int n = s.length();
-        StringBuilder sb = new StringBuilder();
-        // HashMap<Character,Integer>mp = new HashMap<>();
-        // for(int i=0;i<n;i++){
-        //     char ch = s.charAt(i);
-        //     mp.put(ch,mp.getOrDefault(ch,0)+1);
-        // }
-
-        // for(int i=0;i<n;i++){
-        //     char ch = s.charAt(i);
-        //     for(int j=i+1;j<n;j++){
-        //         if(mp.get(ch)<mp.get(s.charAt(j))){
-        //             sb.append(s.charAt(j));
-        //         } 
-        //     }
-        //     //sb.append(ch);
-        // }
-        // return sb.toString();
-
-        int[] fr = new int[256];
-        for(int i=0;i<n;i++){
-            char ch = s.charAt(i);
-            fr[ch]++;
-        }
-        char[] ch = new char[256];
-        for(int i=0;i<256;i++){
-            ch[i] = (char)i;
-        }
-        for(int i=0;i<256;i++){
-            //char ch = s.charAt(i);
-            for(int j = i;j<256;j++){
+    //     StringBuilder sb = new StringBuilder();
+       
+    //     int[] fr = new int[256];
+    //     for(int i=0;i<n;i++){
+    //         char ch = s.charAt(i);
+    //         fr[ch]++;
+    //     }
+    //     char[] ch = new char[256];
+    //     for(int i=0;i<256;i++){
+    //         ch[i] = (char)i;
+    //     }
+    //     for(int i=0;i<256;i++){
+    //         //char ch = s.charAt(i);
+    //         for(int j = i;j<256;j++){
                 
-                if(fr[i]<fr[j] ){
-                    int t = fr[i];
-                    fr[i] = fr[j];
-                    fr[j] = t;
+    //             if(fr[i]<fr[j] ){
+    //                 int t = fr[i];
+    //                 fr[i] = fr[j];
+    //                 fr[j] = t;
 
-                    char x = ch[i];
-                    ch[i] = ch[j];
-                    ch[j] = x;
-                }
-            }
-        }
-       for(int i=0;i<256;i++){
-        while(fr[i]-->0){
-            sb.append(ch[i]);
-        }
-       }
-        return sb.toString();
+    //                 char x = ch[i];
+    //                 ch[i] = ch[j];
+    //                 ch[j] = x;
+    //             }
+    //         }
+    //     }
+    //    for(int i=0;i<256;i++){
+    //     while(fr[i]-->0){
+    //         sb.append(ch[i]);
+    //     }
+    //    }
+    //     return sb.toString();
+    // }
+
+
+    HashMap<Character,Integer>mp = new HashMap<>();
+    for(char ch:s.toCharArray()){
+        mp.put(ch,mp.getOrDefault(ch,0)+1);
     }
+    List<Character>chars = new ArrayList<>(mp.keySet());
+    Collections.sort(chars,(a,b)->mp.get(b)-mp.get(a));
+    StringBuilder sb = new StringBuilder();
+    for(char c:chars){
+        int x = mp.get(c);
+        while(x-->0){
+            sb.append(c);
+        }
+        
+    }
+    return sb.toString();
+}
 }

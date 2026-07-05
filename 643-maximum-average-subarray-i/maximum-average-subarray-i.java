af62444e36512f7d7f8c88616e36e049fb6cd0c8
Priyanshu kumar <152582789+Priyanshukumar23@users.noreply.github.com>
2026-07-05 18:47:57 +0530

    Time: 4 ms (44.02%) | Memory: 69.9 MB (10.28%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -1,10 +1,7 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
         int n = nums.length;
-        HashMap<Integer,Integer>mp = new HashMap<>();
-        for(int i=0;i<n;i++){
-            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
-        }
+        
         double max=Integer.MIN_VALUE;
         int l = 0,s=0;
         for(int i=0;i<k;i++){

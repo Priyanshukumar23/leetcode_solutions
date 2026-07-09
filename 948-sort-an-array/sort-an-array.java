7a51bd466972b17630ab124218996a4e3e5bf695
Priyanshu kumar <152582789+Priyanshukumar23@users.noreply.github.com>
2026-07-09 10:57:08 +0530

    Time: 27 ms (49.98%) | Memory: 86.6 MB (17.62%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,8 +1,36 @@
 class Solution {
-    public int[] sortArray(int[] nums) {
-        int n = nums.length;
-        
-        Arrays.sort(nums);
-        return nums;
+    public int[] sortArray(int[] arr) {
+        int n = arr.length;
+       mergesort(arr,0,n-1);
+       return arr;
+    }
+    public void mergesort(int[] arr,int l,int r){
+        if(l>=r)return;
+        int m = l+(r-l)/2;
+        mergesort(arr,l,m);
+        mergesort(arr,m+1,r);
+        merge(arr,l,m,r);
+    }
+    private void merge(int[] arr,int l,int m,int r){
+        int[] t = new int[r-l+1];
+        int i=l;
+        int j = m+1;
+        int k=0;
+        while(i<=m && j<=r){
+            if(arr[i]<=arr[j]){
+                t[k++] = arr[i++];
+            } else{
+                t[k++] = arr[j++];
+            }
+        }
+        while(i<=m){
+            t[k++] = arr[i++];
+        }
+        while(j<=r){
+            t[k++] = arr[j++];
+        }
+        for(int x=0;x<k;x++){
+            arr[l+x] = t[x];
+        }
     }
 }

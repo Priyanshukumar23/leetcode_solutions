69c707153d22658d5303e23eedca2145438a1282
Priyanshu kumar <152582789+Priyanshukumar23@users.noreply.github.com>
2026-07-20 11:53:26 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (33.71%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -1,22 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        
-        // case 1: both nodes are null
-        if(p == null && q == null){
-            return true;
+        Queue<TreeNode>q1 = new LinkedList<>();
+        Queue<TreeNode>q2 = new LinkedList<>();
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        q1.offer(p);
+        q2.offer(q);
+        if(q1.size()!=q2.size())return false;
+        while(!q1.isEmpty() && !q2.isEmpty()){
+            TreeNode cur = q1.poll();
+            TreeNode pr = q2.poll();
+            if(cur.val!=pr.val)return false;
+            if ((cur.left == null) != (pr.left == null))
+    return false;
+
+if ((cur.right == null) != (pr.right == null))
+    return false;
+            if(cur.left!=null){
+                q1.offer(cur.left);
+            }
+            if(cur.right!=null){
+                q1.offer(cur.right);
+            }
+            if(pr.left!=null){
+                q2.offer(pr.left);
+            }
+            if(pr.right!=null){
+                q2.offer(pr.right);
+            }
         }
-        
-        // case 2: one is null
-        if(p == null || q == null){
-            return false;
-        }
-        
-        // case 3: values not equal
-        if(p.val != q.val){
-            return false;
-        }
-        
-        // case 4: check left and right subtree
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return q1.isEmpty() && q2.isEmpty();
     }
 }
